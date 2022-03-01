7afad8fce915d4e9142282db9a41e02ce3509380
LaSe28 <mattia.las28@gmail.com>
2022-03-01 15:54:24 +0100

    fine esercizio parole palindrome

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/*
+ESERCIZIO PALINDROMI
+*/
+
+// let title = document.querySelector('#title')
+
+// let inputWord = prompt(`dammi una parola`);
+
+// let invert = invertWord(inputWord)
+
+// if(reversedWord == inputWord){
+//     title.innerHTML=`La parola ${inputWord} è palindroma! Ecco il risultato ${reversedWord}`;
+// } else{
+//     title.innerHTML=`La parola ${inputWord} non è palindroma! Ecco il risultato ${reversedWord}`;
+// }
+
+// function invertWord(inputWord){    
+//     let splitWord = inputWord.split('')
+//     let revWord = splitWord.reverse()
+//     reversedWord = revWord.join('')
+//     return reversedWord
+// }
+
+
+/******************************************************************************************
+ESERCIZIO PARI E DISPARI
+*/
+
+let pari = document.querySelector('#even')
+let dispari = document.querySelector('#odd')
+
+
+
+
+
+
+
+
+
+
